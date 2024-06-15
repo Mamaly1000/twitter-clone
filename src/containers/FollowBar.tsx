0e3c70f43f37fc76af344eddb3ba12f0aa805b8c
Mamaly1000 <105161078+Mamaly1000@users.noreply.github.com>
2024-06-15 21:05:50 +0330

    fix followbar container design

--- src/containers/FollowBar.tsx
+++ src/containers/FollowBar.tsx
@@ -10,9 +10,9 @@ const FollowBar = () => {
   const { location } = useUserLocation(user?.id);
   return (
     <ScrollHideShowComponent
-      className="p-4 hidden lg:flex col-span-3 bg-light dark:bg-black items-start justify-start flex-col gap-3  pb-10"
+      className="p-4 md:flex hidden  col-span-1 lg:col-span-3 bg-light dark:bg-black items-start justify-start flex-col gap-3 pb-10"
       targetElement={
-        <div className="scroll-hidden w-full xl:max-w-[90%] flex flex-col gap-5 max-h-screen h-full min-h-full overflow-y-auto overflow-x-hidden pb-5">
+        <div className="scroll-hidden hidden w-full xl:max-w-[90%] lg:flex flex-col gap-5 max-h-screen h-full min-h-full overflow-y-auto overflow-x-hidden pb-5">
           <RecommentUserList />
           <TrendHashtags userLocation={location} />
         </div>
